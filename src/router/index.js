bf094ec85e3dab859be7dd51b224a8ab6fb0fcc4
Jason McKenzie <jmac.atx@gmail.com>
2021-07-21 13:09:02 -0500

    adds signup, login and logout pages and actions

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,18 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import Test from "../views/Test.vue";
 import Signup from "../views/Signup.vue";
 import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: "/", name: "Home", component: Home, },
   { path: "/about", name: "About", component: () => import("../views/About.vue"), },
-  { path: "/test", name: "Test", component: Test, },
   { path: "/signup", name: "Signup", component: Signup,},
-  { path: "/login", name: "login", component: Login },
+  { path: "/login", name: "Login", component: Login },
+  { path: "/logout", name: "Logout", component: Logout },
 ];
 
 const router = new VueRouter({
